fe9b68dc52067330f1b1a0f5caebfc5ac93a907e
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-12 11:59:54 -0400

    basic registration added

--- coco_project/urls.py
+++ coco_project/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('users/', include('users.urls')),
     path('users/',include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
