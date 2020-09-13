b741899c35609192850615e60b779f84fcc9c376
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-13 12:48:38 -0400

    problem create template added

--- problems/views.py
+++ problems/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Problem
+
+class ProblemCreateView(CreateView):
+    model = Problem
