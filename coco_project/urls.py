cb36861a6698f8643e86ffaea7ca16e5669afb44
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-17 09:59:03 -0400

    media configured

--- coco_project/urls.py
+++ coco_project/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('problems/', include('problems.urls')),
     path('comments/', include('comments.urls')),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
