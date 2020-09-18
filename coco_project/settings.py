1198d236189451ca71ab04bdf370d06d6b6896c9
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-18 02:36:21 -0400

    updated allowed hosts

--- coco_project/settings.py
+++ coco_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['hidden-caverns-46090.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
