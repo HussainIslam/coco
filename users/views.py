3675e24cfeca45ba59e5d0ec5c6be60382b1a3ea
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-18 01:21:51 -0400

    added permission to languages, and show problms and lang in navbar only to logged in users

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, ListView
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CustomUserCreationForm, ProgrammingLanguageForm, CustomUserChangeForm
 from .models import ProgrammingLanguage
@@ -10,46 +11,53 @@ class SignupView(CreateView):
     success_url = reverse_lazy('account_login')
     template_name = 'signup.html'
 
-class ProfileView(DetailView):
+class ProfileView(LoginRequiredMixin,DetailView):
     model = get_user_model()
     context_object_name = 'user'
+    login_url = 'account_login'
     template_name = 'users/current_profile.html'
 
     def get_object(self):
         return get_user_model().objects.get(username=self.request.user)
 
-class ProfileUpdateView(UpdateView):
+class ProfileUpdateView(LoginRequiredMixin,UpdateView):
     form_class = CustomUserChangeForm
     context_object_name = 'user'
     success_url = reverse_lazy('profile')
+    login_url = 'account_login'
     template_name = 'users/update_profile.html'
 
     def get_object(self, queryset=None):
         return get_user_model().objects.get(username=self.request.user)
 
-class ProgrammingLanguageListView(ListView):
+class ProgrammingLanguageListView(LoginRequiredMixin,ListView):
     model = ProgrammingLanguage
     context_object_name = 'languages'
+    login_url = 'account_login'
     template_name = 'ProgrammingLanguage/list_pl.html'
 
-class ProgrammingLanguageDetailView(DetailView):
+class ProgrammingLanguageDetailView(LoginRequiredMixin,DetailView):
     model = ProgrammingLanguage
     context_object_name = 'language'
+    login_url = 'account_login'
     template_name = 'ProgrammingLanguage/detail_pl.html'
 
-class ProgrammingLanguageCreate(CreateView):
+class ProgrammingLanguageCreate(LoginRequiredMixin,CreateView):
     form_class = ProgrammingLanguageForm
+    login_url = 'account_login'
     template_name = 'ProgrammingLanguage/create_pl.html'
 
-class ProgrammingLanguageUpdateView(UpdateView):
+class ProgrammingLanguageUpdateView(LoginRequiredMixin,UpdateView):
     model = ProgrammingLanguage
     form_class = ProgrammingLanguageForm
     context_object_name = 'language'
     success_url = reverse_lazy('languages')
+    login_url = 'account_login'
     template_name = 'ProgrammingLanguage/update_pl.html'
 
-class ProgrammingLanguageDeleteView(DeleteView):
+class ProgrammingLanguageDeleteView(LoginRequiredMixin,DeleteView):
     model = ProgrammingLanguage
     success_url = reverse_lazy('languages')
     context_object_name = 'language'
+    login_url = 'account_login'
     template_name = 'ProgrammingLanguage/delete_pl.html'
