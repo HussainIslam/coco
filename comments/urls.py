6a6160ed779689c5503c9c16c6c937ef321f8867
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-19 01:27:30 -0400

    comment update feature added

--- comments/urls.py
+++ comments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import DeleteCommentView
+from .views import DeleteCommentView, CommentUpdateView
 
 urlpatterns = [
     path('<int:pk>/delete/', DeleteCommentView.as_view(), name="delete_comment"),
+    path('<int:pk>/update/', CommentUpdateView.as_view(), name="update_comment"),
 ]
--- comments/views.py
+++ comments/views.py
@@ -5,11 +5,9 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 from problems.models import Problem
 from .models import Comment
+from .forms import CommentForm
 
-class DeleteCommentView(DeleteView):
-    model = Comment
-    template_name = 'Comments/delete_comment.html'
-    
+class CustomCommentMixin(object):
     def get_success_url(self, **kwargs):
         problem_id = self.kwargs['pk']
         cur_problem = Comment.objects.get(id=problem_id).problem
@@ -18,3 +16,17 @@ class DeleteCommentView(DeleteView):
     def test_func(self):
         comment = self.get_object()
         return comment.commenter == self.request.user
+
+class DeleteCommentView(CustomCommentMixin,DeleteView):
+    model = Comment
+    template_name = 'Comments/delete_comment.html'
+    
+    
+
+class CommentUpdateView(CustomCommentMixin,UpdateView):
+    form_class = CommentForm
+    template_name = 'Comments/update_comment.html'
+
+    def get_object(self, **kwargs):
+        return Comment.objects.get(id=self.kwargs['pk'])
+
