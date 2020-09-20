19c5643cd973e946d279e9ee08015778a22c9e2d
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-20 15:33:37 -0400

    started markdown tutorial page

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, MarkdownTutorialPageView
 
 urlpatterns = [
     path('', HomePageView, name='home'),
+    path('markdown-tutorial/', MarkdownTutorialPageView, name="markdown_tutorial")
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def HomePageView(request):
     return render(request, 'home.html')
+
+def MarkdownTutorialPageView(request):
+    return render(request, 'markdown_tutorial.html')
