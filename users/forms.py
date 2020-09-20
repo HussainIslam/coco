b21b3d05245c053f980a9d9bba3adadf7b7c84e4
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-20 19:42:29 -0400

    signup and profile update of dob with datepicker

--- users/forms.py
+++ users/forms.py
@@ -9,16 +9,21 @@ class CustomUserCreationForm(UserCreationForm):
     
     class Meta: 
         model = get_user_model()
-        fields = UserCreationForm.Meta.fields + ('email','avatar', 'profile', 'dob', 'languages',)
+        fields = ( 'username', 'first_name', 'last_name', 'email','avatar', 'profile', 'dob', 'languages',)
+        list_of_years = range(1950, 2020)
+        widgets = {
+            'dob': forms.SelectDateWidget(years=list_of_years)
+        }
 
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
         model = get_user_model()
+        list_of_years = range(1950, 2020)
         fields = ('username', 'first_name', 'last_name', 'email', 'avatar', 'profile', 'dob', 'password')
         #fields = '__all__'
         widgets = {
-            'dob': forms.DateInput()
+            'dob': forms.SelectDateWidget(years=list_of_years)
         }
 
 class ProgrammingLanguageForm(ModelForm):
