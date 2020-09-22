564aea9bd5ee45b77076a88e5813dcdbeec39f12
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-22 07:02:55 -0400

    martor field in body of blog

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
+from martor.fields import MartorFormField
+
 from .models import Blog
 
 class BlogModelForm(forms.ModelForm):
+    body = MartorFormField()
     class Meta:
         model = Blog
         fields = ('title', 'status', 'blog_tags', 'body',)
