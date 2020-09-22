85a32e2071f246de5a0ae95ac62ea6742dbdaa52
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-22 07:35:12 -0400

    stylized blog detail template

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import BlogListView, BlogCreateView, BlogDetailView
+from .views import (BlogListView, 
+                    BlogCreateView, 
+                    BlogDetailView,
+                    BlogUpdateView,)
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="blogs"),
     path('create/', BlogCreateView.as_view(), name='create_blog'),
     path('<int:pk>/', BlogDetailView.as_view(), name="detail_blog"),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name="update_blog"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 
 from .models import Blog
 from .forms import BlogModelForm
@@ -19,3 +19,9 @@ class BlogListView(ListView):
     model = Blog
     context_object_name = 'blogs'
     template_name = 'Blog/list_blogs.html'
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogModelForm
+    context_object_name = 'blog'
+    template_name = 'Blog/update_blog.html'
