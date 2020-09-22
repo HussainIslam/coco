c5c86be433b04ebc2059408e58f1c6d4edab59d9
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-21 23:16:46 -0400

    created model for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from taggit.managers import TaggableManager
+
+class Blog(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default="draft")
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, blank=True, related_name='blogs')
+    blog_tags = TaggableManager()
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("", kwargs={"pk": self.pk})
+    
