4931720d56006379059e151f182a53d55fcc0161
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-28 09:21:10 -0400

    stylized create problem page

--- problems/forms.py
+++ problems/forms.py
@@ -7,6 +7,11 @@ class ProblemModelForm(forms.ModelForm):
         model = Problem
         fields = ['title', 'language', 'status', 'difficulty', 'tags','description', ]
         widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+            'language': forms.Select(attrs={'class': 'custom-select col-sm-2'}),
+            'status': forms.Select(attrs={'class': 'custom-select col-sm-2'}),
+            'difficulty': forms.Select(attrs={'class': 'custom-select col-sm-2'}),
+            'tags': forms.TextInput(attrs={'class': 'form-control col-sm-2', "placeholder": 'comma-seperated tags'}),
             'description': forms.Textarea(attrs={'id': 'textarea', 'novalidate': 'true'}),
         }
         
---
+++ static/scripts/problems.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let title = document.getElementById("id_title");
+    let language = document.getElementById("id_language");
+    let status = document.getElementById("id_status");
+    let difficulty = document.getElementById("id_difficulty");
+    let tags = document.getElementById("id_tags");
+
+
+    
+}
