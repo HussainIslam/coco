d300d24f78e72dac302a2a1567b254df52bdb760
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-28 23:02:53 -0400

    created form for blogcomments

--- comments/forms.py
+++ comments/forms.py
@@ -2,10 +2,15 @@ from django import forms
 
 from martor.fields import MartorFormField
 
-from .models import Comment
+from .models import Comment, BlogComment
 
 class CommentForm(forms.ModelForm):
     body = MartorFormField()
     class Meta:
         model = Comment
+        fields = ('body',)
+
+class BlogCommentForm(forms.ModelForm):
+    class Meta:
+        model = BlogComment
         fields = ('body',)
--- comments/models.py
+++ comments/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from problems.models import Problem
+from blog.models import Blog
 
 class Comment(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE, related_name='comments')
     commenter = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='comments')
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'Comment by { self.commenter.username } on { self.problem.title }'
+
+class BlogComment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="blogcomments")
+    commenter = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True, related_name='blogcomments')
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
