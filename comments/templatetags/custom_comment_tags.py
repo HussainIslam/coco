5b15d0f647bbadc203492f9989b03419fb747541
Md. Hussainul Islam Sajib <m.hussainul.islam@gmail.com>
2020-09-29 08:04:30 -0400

    refactored problem comments with custom tag and style

--- comments/templatetags/custom_comment_tags.py
+++ comments/templatetags/custom_comment_tags.py
@@ -2,6 +2,10 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag("Comments/_single_comment.html")
-def stylized_comment(comment):
+@register.inclusion_tag("Comments/_single_blog_comment.html")
+def stylized_blog_comment(comment):
+    return { 'comment': comment }
+
+@register.inclusion_tag("Comments/_single_problem_comment.html")
+def stylized_problem_comment(comment):
     return { 'comment': comment }
